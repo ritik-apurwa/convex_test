d051df20ed1b535f9ec219b332dd002a6983fc6f
ritik-apurwa <ritikapurwa3500@gmail.com>
2024-07-15 12:52:18 +0530

    userschema and podcast schema added

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
+"use client";
+
+import { useQuery } from "convex/react";
+import { api } from "../convex/_generated/api";
 import Image from "next/image";
-import React from "react";
 
 const page = () => {
   return (
-    <Image
-      src="https://img.freepik.com/free-vector/prototyping-process-concept-illustration_114360-2741.jpg?w=740&t=st=1721025242~exp=1721025842~hmac=d5ba785933ea5ff461a4ae7696f9a369988da83cb9cf14454638ace51f54deea"
-      alt="image"
-      height={400}
-      width={500}
-    />
+    <div>
+      <Image
+        src="https://img.freepik.com/free-vector/prototyping-process-concept-illustration_114360-2741.jpg?w=740&t=st=1721025242~exp=1721025842~hmac=d5ba785933ea5ff461a4ae7696f9a369988da83cb9cf14454638ace51f54deea"
+        alt="image"
+        height={400}
+        width={500}
+      />
+    </div>
   );
 };
 
